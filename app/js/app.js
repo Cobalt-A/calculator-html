13f410e6415e5da4733f0927676bbada0369d60b
Nikita Shkutyak <nikitos@iMac-Nikita.local>
2023-02-14 23:20:44 +0400

    final

--- app/js/app.js
+++ app/js/app.js
@@ -8,77 +8,77 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	const first_pay = document.querySelector('#first_pay');
 	const range_first_pay = document.querySelector('#range_first_pay');
-	const percent = document.querySelector('.range-content__label-percent')
+	const percent = document.querySelector('.range__label-percent');
 
 	const time = document.querySelector('#time');
 	const range_time = document.querySelector('#range_time');
 
-	const fullSum = document.querySelector('.calculator-form__summ-full')
-	const monthsSum = document.querySelector('.calculator-form__summ-months')
+	const fullSum = document.querySelector('.calculator-form__summ-full');
+	const monthsSum = document.querySelector('.calculator-form__summ-months');
 
 
 
-	range_car_price.addEventListener('input', function (e) {
+	range_car_price.addEventListener('input', (e) => {
 		car_price.value = e.target.value;
-		range_first_pay.setAttribute('min', (e.target.value * 10)/100)
-		range_first_pay.setAttribute('max', (e.target.value * 60)/100)
+		range_first_pay.setAttribute('min', (e.target.value * 10)/100);
+		range_first_pay.setAttribute('max', (e.target.value * 60)/100);
 
-		range_first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100)
-		first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100)
+		range_first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100);
+		first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100);
 
-		percent.innerHTML = findPercent(e.target.value, first_pay.value)
+		percent.innerHTML = findPercent(e.target.value, first_pay.value);
 	});
-	car_price.addEventListener('input', function (e) {
+	car_price.addEventListener('input', (e) => {
 		range_car_price.value = e.target.value;
-		range_first_pay.setAttribute('min', (e.target.value * 10)/100)
-		range_first_pay.setAttribute('max', (e.target.value * 60)/100)
+		range_first_pay.setAttribute('min', (e.target.value * 10)/100);
+		range_first_pay.setAttribute('max', (e.target.value * 60)/100);
 
-		range_first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100)
-		first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100)
+		range_first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100);
+		first_pay.value = Math.round((e.target.value * Number(percent.innerHTML))/100);
 
-		percent.innerHTML = findPercent(e.target.value, first_pay.value)
+		percent.innerHTML = findPercent(e.target.value, first_pay.value);
 	});
 
 
-	range_first_pay.addEventListener('input', function (e) {
-		first_pay.value = Math.round(e.target.value)
-		percent.innerHTML = findPercent(Number(car_price.value), first_pay.value)
+	range_first_pay.addEventListener('input', (e) => {
+		first_pay.value = Math.round(e.target.value);
+		percent.innerHTML = findPercent(Number(car_price.value), first_pay.value);
 	});
-	first_pay.addEventListener('input', function (e) {
-		range_first_pay.value = Math.round(e.target.value)
-		percent.innerHTML = findPercent(Number(car_price.value), first_pay.value)
+	first_pay.addEventListener('input', (e) => {
+		range_first_pay.value = Math.round(e.target.value);
+		percent.innerHTML = findPercent(Number(car_price.value), first_pay.value);
 	});
 
 
-	range_time.addEventListener('input', function (e) {
+	range_time.addEventListener('input', (e) => {
 		time.value = e.target.value;
 	});
-	time.addEventListener('input', function (e) {
+	time.addEventListener('input', (e) => {
 		range_time.value = e.target.value;
 	});
 
 
 	document.querySelector('.calculator-form').addEventListener('input', (e) => {
-		const msum = Math.round((Number(car_price.value) - Number(first_pay.value)) * ((0.05 * Math.pow(Number(time.value), 0.05))/(Math.pow(Number(time.value), 0.05))))
-		const fsum = Math.round(Number(first_pay.value) + Number(time.value) * Number(msum))
+		const msum = Math.round((Number(car_price.value) - Number(first_pay.value)) * ((0.05 * Math.pow(Number(time.value), 0.05))/(Math.pow(Number(time.value), 0.05))));
+		const fsum = Math.round(Number(first_pay.value) + Number(time.value) * Number(msum));
 
-		monthsSum.innerHTML = new Intl.NumberFormat('ru-RU').format(msum)
-		fullSum.innerHTML =  new Intl.NumberFormat('ru-RU').format(fsum)
-	})
+		monthsSum.innerHTML = new Intl.NumberFormat('ru-RU').format(msum);
+		fullSum.innerHTML =  new Intl.NumberFormat('ru-RU').format(fsum);
+	});
 
 
 	document.querySelector('.calculator-form__button').addEventListener('click', (e) => {
 
-		const target = e.target.closest('.calculator-form__button')
+		const target = e.target.closest('.calculator-form__button');
 
-		const text = target.querySelector('.calculator-form__button-text')
-		const loader = target.querySelector('.calculator-form__loader')
+		const text = target.querySelector('.calculator-form__button-text');
+		const loader = target.querySelector('.calculator-form__loader');
 
 		console.log(text);
 		console.log(loader);
 
-		text.classList.add('hide')
-		loader.classList.remove('hide')
+		text.classList.add('hide');
+		loader.classList.remove('hide');
 		
 		setTimeout(() => {
 			const obj = {
@@ -90,22 +90,42 @@ document.addEventListener('DOMContentLoaded', () => {
 				fullSum: fullSum.innerHTML.replace(/&nbsp(?:;?)/ig,'\u00A0').replace(/\s/g, "")
 			}
 			const json = JSON.stringify(obj);
+			text.classList.remove('hide');
+			loader.classList.add('hide');
 
-			text.classList.remove('hide')
-			loader.classList.add('hide')
-			target.setAttribute('disabled', '')
+			target.setAttribute('disabled', '');
+			car_price.setAttribute('disabled', '');
+			first_pay.setAttribute('disabled', '');
+			time.setAttribute('disabled', '');
+			range_car_price.setAttribute('disabled', '');
+			range_first_pay.setAttribute('disabled', '');
+			range_time.setAttribute('disabled', '');
 
-			alert(json)			
-		}, 1000)
+			alert(json);
+		}, 1000);
 		
+	});
+
+
+	document.querySelectorAll('.range__input').forEach((el) => {
+		setRange(el)
+		el.addEventListener('input', (e) => {
+			setRange(e.target)
+		})
 	})
 
+	function setRange(el) {
+		let min = el.min,
+			max = el.max,
+			val = el.value;
 
-	function findPercent(fullSum, fistPay) {
-		const result = Math.round((fistPay * 100)/fullSum)
-		return result
+		el.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
 	}
 
+	function findPercent(fullSum, fistPay) {
+		const result = Math.round((fistPay * 100)/fullSum);
+		return result;
+	};
 
 
 })
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
+You may open this html page in 'https://cobalt-a.github.io/calculator-html/dist/'
+
 For instalation packages run: 'npm i'
 
 For running app in localhost:3000: 'npm run start'
